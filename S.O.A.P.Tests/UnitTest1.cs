50fdd4dd65ff6a26af9bb744a5b601eb0450cfec
jeff.corn <jeff.corn@obea.co.uk>
2021-04-12 20:00:57 +0100

    Version set to 0.0.0.1
    Initial Commit

---
+++ S.O.A.P.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace S.O.A.P.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ S.O.A.P/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="S.O.A.P.WebApiApplication" Language="C#" %>
